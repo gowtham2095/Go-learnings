e410d644a16edfa5b57918449cbb0a7020bcf186
Hariharan <hariharan.s@samsung.com>
2020-11-01 16:51:08 +0530

    http-server

---
+++ server-setup/http-server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+)
+
+func main() {
+	li, err := net.Listen("tcp", "localhost:8888")
+	if err != nil {
+		log.Panic(err)
+	}
+	defer li.Close()
+	for {
+		conn, err := li.Accept()
+		if err != nil {
+			log.Println(err)
+		}
+		go handle(conn)
+	}
+}
+
+func handle(conn net.Conn) {
+	// scanner := bufio.NewScanner(conn)
+	// for scanner.Scan() {
+	// 	ln := scanner.Text()
+	// 	fmt.Println(ln)
+	// }
+	// defer conn.Close()
+	request(conn)
+	respond(conn)
+
+}
